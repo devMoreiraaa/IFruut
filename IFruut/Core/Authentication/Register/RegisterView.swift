b874238ac21a1e4e0edc23e5a2ca380758ef60ec
leonardo <leonardoazevedomoreiraa@gmail.com>
2024-03-18 13:27:57 -0300

    Fiz a parte do registro caso alguém não tenha uma conta registrada no nosso aplicativo

--- IFruut/Core/Authentication/Register/RegisterView.swift
+++ IFruut/Core/Authentication/Register/RegisterView.swift
@@ -8,12 +8,109 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var nome = ""
+    @State private var email = ""
+    @State private var senha = ""
+    @State private var numero = ""
     var body: some View {
         NavigationStack{
-            Image(.group87)
-                .resizable()
-                .scaledToFill()
+            
+            VStack {
+                // Imagem principal da tela de registro
+                
+                Image(.group87)
+                    .resizable()
+                    .scaledToFill()
                 .frame(width: 277, height: 198)
+                
+                Text("Faça Seu Cadastro")
+                    .font(.title)
+                    .padding(.top)
+            
+                HStack {
+                    // PARA ESCREVER SEU NOME
+                    Image(systemName: "person.fill")
+                        .foregroundStyle(.verde)
+                        .padding(.leading)
+                    
+                    TextField("Nome", text: $nome)
+                        .bold()
+                        .padding()
+                        
+                }
+                .frame(width: 300, height: 50)
+                .background(Color.white)
+                .cornerRadius(12)
+                .padding()
+                .shadow(color: .black.opacity(0.5),radius: 0, x: 0, y: 4)
+                
+                
+                HStack {
+                    // PARA ESCREVER SEU EMAIL
+                    Image(systemName: "mail.fill")
+                        .foregroundStyle(.verde)
+                        .padding(.leading)
+                    
+                    TextField("Email", text: $email)
+                        .bold()
+                        .padding()
+                        
+                }
+                .frame(width: 300, height: 50)
+                .background(Color.white)
+                .cornerRadius(12)
+                .padding()
+                .shadow(color: .black.opacity(0.5),radius: 0, x: 0, y: 4)
+                
+                
+                HStack {
+                    // PARA ESCREVER SUA SENHA
+                    Image(systemName: "lock.fill")
+                        .foregroundStyle(.verde)
+                        .padding(.leading)
+                    
+                    TextField("Senha", text: $senha)
+                        .bold()
+                        .padding()
+                        
+                }
+                .frame(width: 300, height: 50)
+                .background(Color.white)
+                .cornerRadius(12)
+                .padding()
+                .shadow(color: .black.opacity(0.5),radius: 0, x: 0, y: 4)
+                
+                
+                HStack {
+                    // PARA ESCREVER SEU NÚMERO
+                    Image(systemName: "list.clipboard.fill")
+                        .foregroundStyle(.verde)
+                        .padding(.leading)
+                    
+                    TextField("Numero", text: $numero)
+                        .bold()
+                        .padding()
+                }
+                .frame(width: 300, height: 50)
+                .background(Color.white)
+                .cornerRadius(12)
+                .padding()
+                .shadow(color: .black.opacity(0.5),radius: 0, x: 0, y: 4)
+                
+                //BOTÃO PARA CADASTRO
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Cadastrar ")
+                        .frame(width: 299, height: 44)
+                        .foregroundStyle(.white)
+                        .bold()
+                        .background(Color.verde)
+                        .cornerRadius(12)
+                        .padding(.top)
+                })
+            }
         }
     }
 }
