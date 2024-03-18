999f87fec69f46e30bfcdeb6897f20ad03dedd68
leonardo <leonardoazevedomoreiraa@gmail.com>
2024-03-17 23:52:27 -0300

    codigo do loginView

--- IFruut/Core/Authentication/Login/LoginView.swift
+++ IFruut/Core/Authentication/Login/LoginView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var EntrarComOGoogle = ""
+    @State private var EntrarComOFacebook = ""
+    @State private var EntrarComAApple = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment:.center){
+            
+            Image(.telalogin)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 291, height: 282)
+            
+            
+            Text("ENTRE NA SUA CONTA")
+                .font(.title)
+                .fontWeight(.semibold)
+            
+            
+            VStack(alignment: .center){
+                HStack{
+                    
+                TextField("Entrar Com o Google", text: $EntrarComOGoogle)
+                        .frame(width: 316, height: 54)
+                        .background(Color.white)
+                        .cornerRadius(12)
+                        .shadow(color: .black.opacity(0.6), radius: 2, x: 0, y: 4)
+                    
+                }
+                
+                TextField("Entrar Com o Facebook", text: $EntrarComOFacebook)
+                    .frame(width: 316, height: 54)
+                    .background(Color.white)
+                    .cornerRadius(12)
+                    .shadow(color: .black.opacity(0.6), radius: 2, x: 0, y: 4)
+                
+                
+                
+                TextField("Entrar Com a Apple", text: $EntrarComAApple)
+                    .frame(width: 316, height: 54)
+                    .background(Color.white)
+                    .cornerRadius(12)
+                    .shadow(color: .black.opacity(0.6), radius: 2, x: 0, y: 4)
+                
+                
+                Text("OU")
+                    .padding(.top, 30)
+                
+                
+                VStack {
+                    Button(action: {
+                        //
+                    }, label: {
+                        Text("Crie Uma Conta")
+                            .frame(width: 240, height: 44)
+                            .foregroundColor(.white)
+                            .bold()
+                            .background(Color.verde)
+                            .cornerRadius(20)
+                            .padding(.top)
+                })
+               }
+            }
+        }
     }
 }
 
