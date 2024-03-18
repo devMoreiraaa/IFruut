04a5b7c015eb25a95141c81dae589a42ee9165c1
leonardo <leonardoazevedomoreiraa@gmail.com>
2024-03-18 15:09:09 -0300

    Coloquei um navigationLink aonde vai para a tela de Bem vindo do aplicativo !

---
+++ IFruut/Core/Welcome/View/Etapa1View.swift
@@ -0,0 +1,18 @@
+//
+//  Etapa1View.swift
+//  IFruut
+//
+//  Created by leonardo Moreira on 18/03/24.
+//
+
+import SwiftUI
+
+struct Etapa1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Etapa1View()
+}
