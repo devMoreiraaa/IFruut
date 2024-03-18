5e17a84ccbee2ca2b0d8f63eecc31104c717bf69
leonardo <leonardoazevedomoreiraa@gmail.com>
2024-03-18 12:39:11 -0300

    arrumei meu codigo do login e acrescentei um NavigationLink e NavigationStack que vai para a pagina de resgistro caso o usuário nao tenha conta feita ainda

---
+++ IFruut/Core/Authentication/Register/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  IFruut
+//
+//  Created by leonardo Moreira on 18/03/24.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
