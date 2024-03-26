e63b5d7b8f98068dc94f1857ef66034f5dc57afa
leonardo <leonardoazevedomoreiraa@gmail.com>
2024-03-26 15:04:34 -0300

    iniciei as abas da TabBar  como : CartView, HomeView, SearchView, PersonView

---
+++ IFruut/Core/TabBar/perfil/PersonView.swift
@@ -0,0 +1,18 @@
+//
+//  PersonView.swift
+//  IFruut
+//
+//  Created by leonardo Moreira on 26/03/24.
+//
+
+import SwiftUI
+
+struct PersonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonView()
+}
