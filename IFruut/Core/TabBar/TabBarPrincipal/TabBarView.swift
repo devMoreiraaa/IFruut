a961a313b7d40be36d202e421b5fc2c68f9c8570
leonardo <leonardoazevedomoreiraa@gmail.com>
2024-03-26 15:01:25 -0300

    codigo da TabBar

--- IFruut/Core/TabBar/TabBarPrincipal/TabBarView.swift
+++ IFruut/Core/TabBar/TabBarPrincipal/TabBarView.swift
@@ -9,9 +9,40 @@ import SwiftUI
 
 struct TabBarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+     
+        TabView {
+            Text("ola")
+            
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+            }
+            
+            Text("segunda view")
+            
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Home")
+            }
+            Text("ola")
+            
+                .tabItem {
+                    Image(systemName: "cart")
+                    Text("Carrinha")
+            }
+            
+            Text("ola")
+            
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Home")
+            }
+            
+    
+           }
+        .accentColor(.green)
+        }
     }
-}
 
 #Preview {
     TabBarView()
